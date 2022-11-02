a9a7c8547cbfa0187af03237d0cade1d9d6ce344
Tomáš Veselý <tomas.vesely.cz@gmail.com>
2022-11-02 15:57:36 +0100

    Lesson 3 - while, break, continue

--- 04numbers.py
+++ 04numbers.py
@@ -1,10 +1,24 @@
 total = 0
+count = 0
+minimum = None
+maximum = None
 
 while True:
     entry = input()
     if entry == "":
         break
-    number = int(entry)
+    try:
+        number = int(entry)
+    except ValueError:
+        continue
     total += number
+    count += 1
+    if minimum is None or number < minimum:
+        minimum = number
+    if maximum is None or number > maximum:
+        maximum = number
 
 print("Sum: " + str(total))
+print("Count: " + str(count))
+print("Minimum: " + str(minimum))
+print("Maximum: " + str(maximum))
---
+++ 06sequences.py
@@ -0,0 +1,5 @@
+number = 1
+
+while number <= 100:
+    print(number)
+    number += 1
